9ae8f33d512a5c42db1577fee4b4d4b63d11d75e
Matthias Richter <matthias.h.richter@gmail.com>
2021-12-03 10:57:24 -0800

    added sequence (prg) import

--- helaocore/server/import_processes.py
+++ helaocore/server/import_processes.py
@@ -9,10 +9,10 @@ from helaocore.helper import print_message
 
 def import_processes(world_config_dict: dict, process_path: str = None, server_name: str = ""):
     """Import process functions into environment."""
-    action_lib = {}
+    process_lib = {}
     if process_path is None:
         process_path = world_config_dict.get(
-            "action_process_path", os.path.join("helao", "config", "process")
+            "process_path", os.path.join("helao", "config", "process")
         )
     if not os.path.isdir(process_path):
         print_message(
@@ -20,14 +20,14 @@ def import_processes(world_config_dict: dict, process_path: str = None, server_n
             server_name,
             f"process path {process_path} was specified but is not a valid directory",
         )
-        return action_lib  # False
+        return process_lib  # False
     sys.path.append(process_path)
-    for actlib in world_config_dict["action_libraries"]:
-        tempd = import_module(actlib).__dict__
-        action_lib.update({func: tempd[func] for func in tempd["SEQUENCES"]})
+    for prclib in world_config_dict["process_libraries"]:
+        tempd = import_module(prclib).__dict__
+        process_lib.update({func: tempd[func] for func in tempd["PROCESSES"]})
     print_message(
         world_config_dict,
         server_name,
-        f"imported {len(world_config_dict['action_libraries'])} processes specified by config.",
+        f"imported {len(world_config_dict['process_libraries'])} processes specified by config.",
     )
-    return action_lib  # True
+    return process_lib
---
+++ helaocore/server/import_sequences.py
@@ -0,0 +1,33 @@
+__all__ = ["import_sequences"]
+
+import os
+import sys
+from importlib import import_module
+
+from helaocore.helper import print_message
+
+
+def import_sequences(world_config_dict: dict, sequence_path: str = None, server_name: str = ""):
+    """Import sequence functions into environment."""
+    sequence_lib = {}
+    if sequence_path is None:
+        sequence_path = world_config_dict.get(
+            "sequence_path", os.path.join("helao", "config", "sequence")
+        )
+    if not os.path.isdir(sequence_path):
+        print_message(
+            world_config_dict,
+            server_name,
+            f"sequence path {sequence_path} was specified but is not a valid directory",
+        )
+        return sequence_lib
+    sys.path.append(sequence_path)
+    for prglib in world_config_dict["sequence_libraries"]:
+        tempd = import_module(prglib).__dict__
+        sequence_lib.update({func: tempd[func] for func in tempd["PROCESSES"]})
+    print_message(
+        world_config_dict,
+        server_name,
+        f"imported {len(world_config_dict['sequence_libraries'])} sequences specified by config.",
+    )
+    return sequence_lib
