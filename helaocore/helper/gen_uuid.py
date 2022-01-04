dbbc9b99ad2a6983bd41ead99b282659eabb538d
Matthias Richter <matthias.h.richter@gmail.com>
2022-01-04 09:47:19 -0800

    Update gen_uuid.py

--- helaocore/helper/gen_uuid.py
+++ helaocore/helper/gen_uuid.py
@@ -3,12 +3,13 @@ __all__ = ["gen_uuid"]
 import time
 from typing import Optional
 
-import shortuuid
-
+# import shortuuid
+import uuid
 
 def gen_uuid(label: str, timestamp: Optional[str] = None, trunc: int = 8):
     "Generate a uuid, encode with larger character set, and trucate."
-    if timestamp is None:
-        timestamp = time.monotonic_ns()
-    short = shortuuid.uuid(name=f"{label}_{timestamp}")[:trunc]
-    return short
+    # if timestamp is None:
+    #     timestamp = time.monotonic_ns()
+    # short = shortuuid.uuid(name=f"{label}_{timestamp}")#[:trunc]
+    # return short
+    return uuid.uuid4()
