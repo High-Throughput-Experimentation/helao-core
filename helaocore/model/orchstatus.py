84b430118d9180ce7d32ba7fd0978447c4ae16d6
Matthias Richter <matthias.h.richter@gmail.com>
2022-02-10 17:02:58 -0800

    Create orchstatus.py

---
+++ helaocore/model/orchstatus.py
@@ -0,0 +1,17 @@
+__all__ = [
+           "OrchStatus"
+          ]
+
+from enum import Enum
+
+
+class OrchStatus(str, Enum):
+    idle = "idle"
+    error = "error"
+    busy = "busy"
+    started = "started"
+    estop = "estop"
+    stopped = "stopped"
+    skip = "skip"
+    stop = "stop"
+    none = "none"
