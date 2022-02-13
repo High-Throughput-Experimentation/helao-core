39925b8b092b099b758b4044b913b75d84d87390
Matthias Richter <matthias.h.richter@gmail.com>
2022-02-12 20:13:56 -0800

    moved action_start_conditions

--- helaocore/model/action_start_condition.py
+++ helaocore/model/action_start_condition.py
@@ -1,9 +1,9 @@
-__all__ = ["action_start_condition"]
+__all__ = ["ActionStartCondition"]
 
 from enum import Enum
 
 
-class action_start_condition(int, Enum):
+class ActionStartCondition(int, Enum):
     no_wait = 0  # orch is dispatching an unconditional action
     wait_for_endpoint = 1  # orch is waiting for endpoint to become available
     wait_for_server = 2  # orch is waiting for server to become available
