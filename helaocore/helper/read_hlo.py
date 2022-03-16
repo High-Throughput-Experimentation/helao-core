7039c9be4977de80d0d2f3fc76648d483b596843
onepunchdan <onepunchdan@users.noreply.github.com>
2022-03-16 14:02:08 -0700

    Add ruamel.yaml to env, load with yaml 1.2 spec

--- helaocore/helper/read_hlo.py
+++ helaocore/helper/read_hlo.py
@@ -1,25 +1,31 @@
 __all__ = ["read_hlo"]
 
 import json
-import yaml
+from ruamel.yaml import YAML
 from pathlib import Path
 from typing import Tuple
 from collections import defaultdict
+
 
 def read_hlo(path: str) -> Tuple[dict, dict]:
     "Parse .hlo file into tuple of dictionaries containing metadata and data."
     path_to_hlo = Path(path)
     with path_to_hlo.open() as f:
         lines = f.readlines()
-        
+
     sep_index = lines.index('%%\n')
-    
-    meta = yaml.load("".join(lines[:sep_index]), Loader=yaml.CLoader)
+
+    yaml = YAML(typ="safe")
+    meta = yaml.load("".join(lines[:sep_index]))
 
     data = defaultdict(list)
-    for line in lines[sep_index+1:]:
+    for line in lines[sep_index + 1 :]:
         line_dict = json.loads(line)
-        for k,v in line_dict.items():
-            data[k] += v
-    
+        print(line_dict)
+        for k, v in line_dict.items():
+            if isinstance(v, list):
+                data[k] += v
+            else:
+                data[k].append(v)
+
     return meta, data
