4a91f4d2a45ebd9258aac5eb551eb0b3ece912ae
Matthias Richter <matthias.h.richter@gmail.com>
2022-03-22 08:47:45 -0700

    Create helaodirs.py

---
+++ helaocore/model/helaodirs.py
@@ -0,0 +1,18 @@
+__all__ = ["HelaoDirs"]
+
+from pydantic import BaseModel
+from pathlib import Path
+from typing import Optional
+
+from ..helper.helaodict import HelaoDict
+
+
+class HelaoDirs(BaseModel, HelaoDict):
+    root: Optional[Path] = None
+    save_root: Optional[Path] = None
+    log_root: Optional[Path] = None
+    states_root: Optional[Path] = None
+    db_root: Optional[Path] = None
+    user_exp: Optional[Path] = None
+    user_seq: Optional[Path] = None
+    
