ab1ba03fd796918cb43b3dc6d6117fe703bb2341
onepunchdan <onepunchdan@users.noreply.github.com>
2022-04-04 13:17:11 -0700

    Fix incomplete rcp_to_dict() function

--- helaocore/data/legacy.py
+++ helaocore/data/legacy.py
@@ -165,6 +165,44 @@ class HTELegacyAPI:
                     lvl = _tab_level(l)
                     dlist.append({"name": k.strip(), "value": v.strip(), "level": lvl})
 
+        def ttree_to_json(ttree, level=0):
+            result = {}
+            for i in range(0, len(ttree)):
+                cn = ttree[i]
+                try:
+                    nn = ttree[i + 1]
+                except:
+                    nn = {"level": -1}
+
+                # Edge cases
+                if cn["level"] > level:
+                    continue
+                if cn["level"] < level:
+                    return result
+                # Recursion
+                if nn["level"] == level:
+                    dict_insert_or_append(result, cn["name"], cn["value"])
+                elif nn["level"] > level:
+                    rr = ttree_to_json(ttree[i + 1 :], level=nn["level"])
+                    dict_insert_or_append(result, cn["name"], rr)
+                else:
+                    dict_insert_or_append(result, cn["name"], cn["value"])
+                    return result
+            return result
+
+        def dict_insert_or_append(adict, key, val):
+            """Insert a value in dict at key if one does not exist
+            Otherwise, convert value to list and append
+            """
+            if key in adict:
+                if type(adict[key]) != list:
+                    adict[key] = [adict[key]]
+                adict[key].append(val)
+            else:
+                adict[key] = val
+
+        return ttree_to_json(dlist)
+
     def getplatemappath_plateid(
         self,
         plateid: int,
@@ -174,18 +212,21 @@ class HTELegacyAPI:
         pmidstr=None,
     ):
         pmfold = self.tryprependpath(self.PLATEMAPFOLDERS, "")
+        self.base.print_message(f"PM folder is {pmfold}", info=True)
         p = None
         if pmidstr is None:
             pmidstr = ""
             infop = self.getinfopath_plateid(plateid)
             if infop is None:
-                if not erroruifcn is None:
+                self.base.print_message("getinfopath_plateid returned None", info=True)
+                if erroruifcn is not None:
                     p = erroruifcn("", self.tryprependpath(self.PLATEMAPFOLDERS, ""))
                 return (p, pmidstr) if return_pmidstr else p
+            self.base.print_message(f"reading {infop}", info=True)
             with open(infop, mode="r") as f:
                 s = f.read(1000)
-
-            if pmfold == "" or (not infokey in s and not "prints" in s):
+            if pmfold == "" or (infokey not in s and "prints" not in s):
+                self.base.print_message(f"PM folder is '' or info has no print.", info=True)
                 if not erroruifcn is None:
                     p = erroruifcn("", self.tryprependpath(self.PLATEMAPFOLDERS, ""))
                 return (p, pmidstr) if return_pmidstr else p
