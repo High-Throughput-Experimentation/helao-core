24cf0107ec72aac5989e43dd0227547d7a4eee5f
kkanatcit <103452480+kkanatcit@users.noreply.github.com>
2022-05-12 15:34:42 -0700

    corrected electrolyte type

--- helaocore/data/sample.py
+++ helaocore/data/sample.py
@@ -422,7 +422,7 @@ class LiquidSampleAPI(_BaseSampleAPI):
         super().__init__(
             sampleclass=LiquidSample(),
             Serv_class=Serv_class,
-            extra_columns="volume_ml REAL NOT NULL, ph REAL, dilution_factor REAL NOT NULL, electrolyte REAL",
+            extra_columns="volume_ml REAL NOT NULL, ph REAL, dilution_factor REAL NOT NULL, electrolyte TEXT",
         )
 
     async def _key_checks(self, sample):
