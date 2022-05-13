fdd67269117c6d56d99c58701b867a8d04d6b271
onepunchdan <onepunchdan@users.noreply.github.com>
2022-05-13 08:41:30 -0700

    Allow more numeric string vals in operator params

--- helaocore/helper/to_json.py
+++ helaocore/helper/to_json.py
@@ -3,9 +3,24 @@ __all__ = ["to_json"]
 import json
 
 
+def fix_numerics(val):
+    if isinstance(val, str):
+        cleaned = val.lower().strip().lstrip('-').replace('.', '', 1).replace('e-', '', 1).replace('e', '', 1)
+        if cleaned.isdigit():
+            retval = float(cleaned)
+            return retval
+    elif isinstance(val, list):
+        retval = [fix_numerics(x) for x in val]
+        return retval
+    elif isinstance(val, dict):
+        retval = {k: fix_numerics(v) for k, v in val.items()}
+        return retval
+    return val
+
+
 def to_json(v):
     try:
         val = json.loads(v.replace("'", '"'))
     except ValueError:
         val = v
-    return val
+    return fix_numerics(val)
