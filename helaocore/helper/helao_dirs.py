f5c5a23a55361e7596e00b220a3968c587ffc06d
Dan Guevarra <onepunchdan@users.noreply.github.com>
2022-06-14 11:24:42 -0700

    use compression for zipped logs

--- helaocore/helper/helao_dirs.py
+++ helaocore/helper/helao_dirs.py
@@ -67,7 +67,7 @@ def helao_dirs(world_cfg: dict, server_name: str) -> HelaoDirs:
         timestamp = re.findall('[0-9]{2}:[0-9]{2}:[0-9]{2}', line0)[0].replace(":","")
         zipname = old_log.replace(".txt", f"{timestamp}.zip")
         arcname = os.path.basename(old_log).replace(".txt", f"{timestamp}.txt")
-        with zipfile.ZipFile(zipname, "w") as zf:
+        with zipfile.ZipFile(zipname, "w", compression=zipfile.ZIP_DEFLATED) as zf:
             zf.write(old_log, arcname)
         os.remove(old_log)
 
