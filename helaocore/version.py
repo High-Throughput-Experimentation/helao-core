e574bcfa5f3e17bbbbaffeacbadd38349dc9b668
onepunchdan <onepunchdan@users.noreply.github.com>
2022-08-15 16:33:20 -0700

    chore: bump version for new run_use attr

--- helaocore/version.py
+++ helaocore/version.py
@@ -1,7 +1,7 @@
 __all__ = ["hlo_version", "get_hlo_version"]
 
 # version number, gets written into every exp/prg and hlo file
-hlo_version = "2022.06.22"
+hlo_version = "2022.08.15"
 
 
 def get_hlo_version():
