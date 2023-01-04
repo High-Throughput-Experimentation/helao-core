0ce2c0ed2fac889418fc2a5e199a114b870466c3
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-01-04 09:47:54 -0800

    chore: version update for new release

--- helaocore/version.py
+++ helaocore/version.py
@@ -1,7 +1,7 @@
 __all__ = ["hlo_version", "get_hlo_version"]
 
 # version number, gets written into every exp/prg and hlo file
-hlo_version = "2022.08.15"
+hlo_version = "2023.01.04"
 
 
 def get_hlo_version():
