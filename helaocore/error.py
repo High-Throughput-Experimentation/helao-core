211f2c34fef1973d1bc986a14717e840b9995cb1
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-02-09 13:48:01 -0800

    fix: make error enum same as string repr

--- helaocore/error.py
+++ helaocore/error.py
@@ -5,11 +5,11 @@ from enum import Enum
 
 class ErrorCodes(str, Enum):
     none = "none"
-    critical = "critical_error"
+    critical = "critical"
     start_timeout = "start_timeout"
     continue_timeout = "continue_timeout"
     done_timeout = "done_timeout"
-    in_progress = "already_in_progress"
+    in_progress = "in_progress"
     not_available = "not_available"
     ssh_error = "ssh_error"
     not_initialized = "not_initialized"
